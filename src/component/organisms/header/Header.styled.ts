e9899213c12d44e4878ab4c67fe05cabee7c7f31
merlin.ho <merlin.ho@kakakocorp.com>
2020-02-10 18:37:25 +0900

    Modify header width 수정

--- src/component/organisms/header/Header.styled.ts
+++ src/component/organisms/header/Header.styled.ts
@@ -1,7 +1,7 @@
 export const wrapHeaderStyle = {
   position: "fixed",
   width: "100%",
-  minWidth: "1280px",
+  minWidth: ["100%", "100%", "100%", "1280px"],
   height: "55px",
   borderBottom: "1px solid rgba(34, 36, 38, 0.15)",
   bg: "#282c34",
@@ -9,7 +9,7 @@ export const wrapHeaderStyle = {
 };
 
 export const headerStyle = {
-  width: "1280px",
+  width: ["100%", "100%", "100%", "1280px"],
   height: "100%",
   px: 3,
   mx: "auto"
